77613e04e3c20fe9a0e7c615ac5d69b519322183
Mateusz <matt.borowczyk@gmail.com>
2025-07-19 20:35:46 +0200

    hide content if not logged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,35 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic"; // to avoid caching of the page as we are fetching data from the database
 
-export default async function HomePage() {
+export async function Images() {
   const photos = await db.query.images.findMany({
     orderBy: (images, { desc }) => [desc(images.createdAt)],
   });
+  return (
+    <div className="flex flex-wrap gap-4">
+      {photos.map((photo) => (
+        <div key={photo.id} className="w-48">
+          <img src={photo.url} alt={photo.name} />
+        </div>
+      ))}
+    </div>
+  )
+}
 
+export default function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {photos.map((photo) => (
-          <div key={photo.id} className="w-48">
-            <img src={photo.url} alt={photo.name} />
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h1 className="text-4xl font-bold">Welcome to the Photo Vault</h1>
+          <p className="text-lg">Please sign in to view the gallery</p>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
