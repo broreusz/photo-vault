22cd9dc87a357759e70b42c8303151585d4e40b2
Mateusz <matt.borowczyk@gmail.com>
2025-07-19 13:33:15 +0200

    typescript version update, set home page to be dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import { db } from "~/server/db";
+
+export const dynamic = "force-dynamic"; // to avoid caching of the page as we are fetching data from the database
+
 const mockedUrls = [
   "https://drmzzs0j0n.ufs.sh/f/VampSEkOiBfAeQZll4pFOdJwZPAskgrcV01fB8lCURqSIDGu",
   "https://drmzzs0j0n.ufs.sh/f/VampSEkOiBfAtx3eAFOhw8iE9LHDBdxfcQv5XUpaCgqrJjPS",
@@ -10,12 +14,14 @@ const mockedPhotos = mockedUrls.map((url) => ({
   id: url.split("/").pop(),
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const photos = await db.query.posts.findMany();
+  console.log(photos);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockedPhotos, ...mockedPhotos, ...mockedPhotos].map((photo) => (
-          <div key={photo.id} className="w-48">
+        {[...mockedPhotos, ...mockedPhotos, ...mockedPhotos].map((photo, index) => (
+          <div key={(photo.id || "mocked-") + index} className="w-48">
             <img src={photo.url} alt={"Image-" + photo.id} />
           </div>
         ))}
