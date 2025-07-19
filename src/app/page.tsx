174756083178539baf3829ea47206f8ff37e7637
Mateusz <matt.borowczyk@gmail.com>
2025-07-19 12:11:05 +0200

    added mocked photos + change styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-import Link from "next/link";
+const mockedUrls = [
+  "https://drmzzs0j0n.ufs.sh/f/VampSEkOiBfAeQZll4pFOdJwZPAskgrcV01fB8lCURqSIDGu",
+  "https://drmzzs0j0n.ufs.sh/f/VampSEkOiBfAtx3eAFOhw8iE9LHDBdxfcQv5XUpaCgqrJjPS",
+  "https://drmzzs0j0n.ufs.sh/f/VampSEkOiBfA2dAIz6MJVLyhOE4U9NWrC6o3uMI5ae7QBpFR",
+  "https://drmzzs0j0n.ufs.sh/f/VampSEkOiBfAcFVX2rPYav0tLEfRlOCdWTyb3KmUws1hMpig",
+]
+
+const mockedPhotos = mockedUrls.map((url) => ({
+  url,
+  id: url.split("/").pop(),
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <p> Hello (gallery in progress)</p>
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockedPhotos, ...mockedPhotos, ...mockedPhotos].map((photo) => (
+          <div key={photo.id} className="w-48">
+            <img src={photo.url} alt={"Image-" + photo.id} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
