f560fa48ba14768fa70e04a3992b84efd7442505
Mateusz <matt.borowczyk@gmail.com>
2025-07-20 22:40:39 +0200

    add upload button with refreshin after upload is done

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import { SignedOut, SignedIn, SignInButton, UserButton } from "@clerk/nextjs";
+import { UploadButton } from "~/utils/uploadthing";
+import { useRouter } from "next/navigation";
 
 export default function TopNav() {
+    const router = useRouter();
+
     return (
         <nav className="w-full flex items-center justify-between border-b p-4 text-xl font-semibold ">
             <div className="flex items-center gap-2 ">
@@ -12,6 +18,9 @@ export default function TopNav() {
                     <SignInButton />
                 </SignedOut>
                 <SignedIn>
+                    <UploadButton endpoint="imageUploader" onClientUploadComplete={() => {
+                        router.refresh();
+                    }} />
                     <UserButton />
                 </SignedIn>
             </div>
