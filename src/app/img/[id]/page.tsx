7fb1441e685cdde7620b8755ff14084a8c094a19
Mateusz <matt.borowczyk@gmail.com>
2025-07-22 19:03:41 +0100

    force dynamic on photo page

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,5 +1,7 @@
 import FullImageView from "~/components/full-image-view";
 
+export const dynamic = "force-dynamic"; // to avoid caching of the page as we are fetching data from the database
+
 export default async function PhotoPage({
   params,
 }: {
