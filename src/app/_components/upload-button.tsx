8900f21b90f14bd463c8abf762b631b5603d5700
Mateusz <matt.borowczyk@gmail.com>
2025-07-23 20:43:56 +0100

    improve toast

--- src/app/_components/upload-button.tsx
+++ src/app/_components/upload-button.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { toast } from "sonner";
 import { useUploadThing } from "~/utils/uploadthing";
 
@@ -30,17 +31,35 @@ const useUploadThingInputProps = (...args: Input) => {
     };
 };
 
+function LoadingSpinner() {
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity=".25" />
+            <path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" className="spinner_ajPY" />
+        </svg>
+    )
+}
+
 export default function UploadButton() {
     const router = useRouter();
 
     const { inputProps, isUploading } = useUploadThingInputProps("imageUploader", {
-        onUploadBegin: (file) => {
-            toast.info(`Uploading ${file}...`);
+        onUploadBegin: () => {
+            toast.info(<div className="flex items-center gap-2">
+                {LoadingSpinner()}
+                Uploading...
+            </div>, {
+                description: "This may take a while...",
+                id: "upload-begin",
+            });
+
         },
         onUploadError: (error: Error) => {
             toast.error(`Error uploading ${error.message}`);
         },
         onClientUploadComplete: () => {
+            toast.dismiss("upload-begin");
+            toast.success("Upload complete!");
             router.refresh();
         }
     });
